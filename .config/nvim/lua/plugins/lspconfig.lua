4273d7959b5b94f2686bd4c28dec9a11d55a0409
Nathan Partridge <nathanpartridge11@gmail.com>
2025-04-27 01:07:30 -0700

    move to new sign config method

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -15,15 +15,34 @@ return {
                 require("lspconfig")[server_name].setup {
                     require("coq").lsp_ensure_capabilities(),
                     settings = {
-                        Lua = { diagnostics = { globals = { 'vim' } } },
+                        Lua = { diagnostics = { globals = { "vim" } } },
                     }
                 }
             end,
         }
-        vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-        vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-        vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-        vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = "",
+                    [vim.diagnostic.severity.WARN] = "",
+                    [vim.diagnostic.severity.INFO] = "",
+                    [vim.diagnostic.severity.HINT] = "󰌵",
+                },
+                linehl = {
+                    [vim.diagnostic.severity.ERROR] = "Error",
+                    [vim.diagnostic.severity.WARN] = "Warn",
+                    [vim.diagnostic.severity.INFO] = "Info",
+                    [vim.diagnostic.severity.HINT] = "Hint",
+                },
+                numhl = {
+                    [vim.diagnostic.severity.ERROR] = "Error",
+                    [vim.diagnostic.severity.WARN] = "Warn",
+                    [vim.diagnostic.severity.INFO] = "Info",
+                    [vim.diagnostic.severity.HINT] = "Hint",
+                }
+            }
+        })
 
         vim.g.coq_settings = { ["clients.lsp.weight_adjust"] = 1.5 }
         vim.g.coq_settings = { ["clients.lsp.resolve_timeout"] = 0.75 }
