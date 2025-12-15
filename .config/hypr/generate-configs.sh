764702095799470d579d5a01c04e515b0167006a
Nathan Partridge <nathanpartridge11@gmail.com>
2025-12-15 00:22:20 -0700

    primary display, add hyprpaper

--- .config/hypr/generate-configs.sh
+++ .config/hypr/generate-configs.sh
@@ -9,9 +9,12 @@ THEME_JSON=~/.config/themes/colors/${THEME}.json
 gomplate -d theme=${THEME_JSON} -f ~/.config/waybar/template.config.jsonc -o /tmp/waybar-config.jsonc
 gomplate -d theme=${THEME_JSON} -f ~/.config/waybar/template.style.css -o /tmp/waybar-style.css
 gomplate -d theme=${THEME_JSON} -f ~/.config/hypr/template.hyprlock.conf -o /tmp/hyprlock.conf
+gomplate -d theme=${THEME_JSON} -f ~/.config/hypr/template.hyprpaper.conf -o /tmp/hyprpaper.conf
 gomplate -d theme=${THEME_JSON} -f ~/.config/mako/template.config -o /tmp/mako-config
 
 pkill waybar
 waybar --config /tmp/waybar-config.jsonc --style /tmp/waybar-style.css &
+pkill hyprpaper
+hyprpaper --config /tmp/hyprpaper.conf &
 pkill mako
 mako --config /tmp/mako-config &
