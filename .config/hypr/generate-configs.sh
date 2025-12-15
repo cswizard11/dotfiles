7ae442c3acf236f0ab0d91337e4ca2eb11a8a026
Nathan Partridge <nathan@inapeartree.net>
2025-12-14 19:06:45 -0700

    make startup app happen after config is generated, allow reload with hyprland

--- .config/hypr/generate-configs.sh
+++ .config/hypr/generate-configs.sh
@@ -8,3 +8,8 @@ source ~/.config/themes/colors/${DOTFILES_THEME}.env
 envsubst < ~/.config/waybar/template.config.jsonc > /tmp/waybar-config.jsonc
 envsubst < ~/.config/waybar/template.style.css > /tmp/waybar-style.css
 envsubst < ~/.config/mako/template.config > /tmp/mako-config
+
+pkill waybar
+waybar --config /tmp/waybar-config.jsonc --style /tmp/waybar-style.css &
+pkill mako
+mako --config /tmp/mako-config &
