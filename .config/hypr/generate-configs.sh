0247aaad1cdd86d3c8fc203d9b532f0df0197fc4
Nathan Partridge <nathan@inapeartree.net>
2025-12-14 18:24:24 -0700

    update waybar to envsubst

--- .config/hypr/generate-configs.sh
+++ .config/hypr/generate-configs.sh
@@ -4,5 +4,7 @@
 # Source theme colors
 source ~/.config/themes/colors/${DOTFILES_THEME}.env
 
-# Generate mako config
+# Generate configs
+envsubst < ~/.config/waybar/template.config.jsonc > /tmp/waybar-config.jsonc
+envsubst < ~/.config/waybar/template.style.css > /tmp/waybar-style.css
 envsubst < ~/.config/mako/template.config > /tmp/mako-config
